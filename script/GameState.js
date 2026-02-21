815bde72c6ba18d4e25868bc8ef37abf53981ce4
Kumasan <dec11nagisa@outlook.com>
2026-02-21 09:47:18 +0900

    Create GameState.js

---
+++ script/GameState.js
@@ -0,0 +1,27 @@
+let player = {
+    stars: new BigNum(1, 1),
+    generators: [],
+    sacrificeMult: new BigNum(1, 0),
+    permanentPower: new BigNum(1, 0),
+    boostLevel: 0,
+    boostMult: new BigNum(1, 0),
+    ip: 0,
+    hasInf: false,
+    lastUpdate: Date.now()
+};
+
+function initPlayer(isInfReset = false) {
+    player.stars = new BigNum(1, 1);
+    player.sacrificeMult = new BigNum(1, 0);
+    player.boostLevel = 0;
+    player.boostMult = new BigNum(1, 0);
+    if (isInfReset) player.permanentPower = new BigNum(1, 0);
+    
+    player.generators = GEN_CONFIG.map(c => ({
+        amount: new BigNum(0, 0),
+        cost: new BigNum(c.base, 0),
+        costMult: new BigNum(c.m, 0),
+        prodMult: new BigNum(1, 0)
+    }));
+    player.generators[0].amount = new BigNum(1, 0);
+}
