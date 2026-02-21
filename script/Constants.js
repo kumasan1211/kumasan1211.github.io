fe5643a52416ce6798ff3db8095fa866dd6c9e7e
Kumasan <dec11nagisa@outlook.com>
2026-02-21 09:46:59 +0900

    Create Constants.js

---
+++ script/Constants.js
@@ -0,0 +1,7 @@
+const GEN_CONFIG = [
+    { base: 2, m: 2 }, { base: 16, m: 4 }, { base: 512, m: 8 }, { base: 65536, m: 16 },
+    { base: 3.3e7, m: 32 }, { base: 6.8e10, m: 64 }, { base: 5.6e14, m: 128 }, { base: 1.8e19, m: 256}
+];
+
+const PRESTIGE_THRESHOLD = 30; // e30
+const INFINITY_THRESHOLD = 308.2547; // 1.79e308
